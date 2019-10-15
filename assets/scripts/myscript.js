04caf32a38d712179ea1af4a308a7ac37c258032
Ruth <ruthale@gmail.com>
2019-10-15 17:31:50 -0300

    seventh commit precosto contact.html

--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -19,3 +19,18 @@ $(function(){
   $('[data-toggle="popover"]').popover();
 
 });
+
+
+//Filtro
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
+});
